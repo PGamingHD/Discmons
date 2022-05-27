8b508a5b5771a6468d6b373822649c3a28d9ccdc
PGamingHD <pontus.2003@hotmail.com>
2022-05-28 00:54:40 +0200

    added ping cmd
    added ping cmd

---
+++ SlashCommands/General Commands/ping.js
@@ -0,0 +1,60 @@
+    const {
+        Client,
+        CommandInteraction,
+        MessageEmbed,
+        MessageActionRow,
+        MessageButton,
+        EmbedBuilder
+    } = require('discord.js');
+    const ee = require('../../botconfig/embed.json');
+    const emoji = require('../../botconfig/embed.json')
+    const prettyMilliseconds = require('pretty-ms');
+    const config = require('../../botconfig/config.json');
+    const dev = require("../../schemas/developerData");
+
+    module.exports = {
+        name: 'ping',
+        description: 'Get the current Client & API ping.',
+        /** 
+         * @param {Client} client 
+         * @param {Message} message 
+         * @param {String[]} args 
+         */
+        run: async (client, interaction, args) => {
+
+            const timeBefore = new Date().getTime();
+            await dev.findOne({
+                developerAccess: "accessStringforDeveloperOnly"
+            })
+            const timeAfter = new Date().getTime();
+            const evaled = timeAfter - timeBefore;
+
+            return interaction.reply({
+                embeds: [
+                    new EmbedBuilder()
+                    .setColor(ee.color)
+                    .setAuthor({
+                        name: `Pong`,
+                        iconURL: client.user.displayAvatarURL()
+                    })
+                    .addFields([{
+                        name: 'Bot Latency',
+                        value: `\`\`\`re\n[ ${Math.floor((Date.now() - interaction.createdTimestamp) - 2 * Math.floor(client.ws.ping))}ms ]\`\`\``,
+                        inline: true
+                    }, {
+                        name: 'API Latency',
+                        value: `\`\`\`re\n[ ${Math.floor(client.ws.ping)}ms ]\`\`\``,
+                        inline: true
+                    }, {
+                        name: 'Database Latency',
+                        value: `\`\`\`re\n[ ${evaled}ms ]\`\`\``
+                    }])
+                    .setTimestamp()
+                    .setFooter({
+                        text: `Requested by ${interaction.user.username}`,
+                        iconURL: interaction.user.displayAvatarURL()
+                    })
+                ]
+            })
+        }
+    }
